13fda22ef435692dbf5c38ab2892c1bc4e796acd
Daniel Rojas <daniel.drojas@outlook.com>
2020-12-16 12:51:38 -0500

    testing server:

--- src/server.js
+++ src/server.js
@@ -4,15 +4,21 @@ import path from 'path';
 
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 app.use(express.static('client/build'));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + 'client/build/index.html'));
+    });
+
 }
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json());
+
 
 const PORT = process.env.PORT || 8000;
 
